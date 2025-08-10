5abcf3d5af178d476289ae5e55341f464b525e7e
LaM0uette <LaM0uette.dev@gmail.com>
2025-08-10 17:26:42 +0200

    clean solution

---
+++ WaitingScreenTerminal/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace WaitingScreenTerminal;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
